7b8d18bc49b7e80c0a783341efb3eb4896772db0
xwinkX <opalyuk.e88@ukr.net>
2022-10-21 00:19:48 +0300

    qwqwqw

--- helpers/index.js
+++ helpers/index.js
@@ -1,7 +1,9 @@
 const RequestError = require("./RequestError");
 const ctrlWrapper = require("./ctrlWrapper");
+const hendleSaveerrors = require("./handleSaveErrors");
 
 module.exports = {
   RequestError,
   ctrlWrapper,
+  hendleSaveerrors,
 };
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -4,24 +4,32 @@ const ctrl = require("../../controllers/contacts");
 
 const { ctrlWrapper } = require("../../helpers");
 
-const { validateBody } = require("../../middlewares");
+const { validateBody, isValidId } = require("../../middlewares");
 
-const schemas = require("../../schemas/contacts");
+const { schemas } = require("../../models/contact");
 
 const router = express.Router();
 
 router.get("/", ctrlWrapper(ctrl.getAll));
 
-router.get("/:id", ctrlWrapper(ctrl.getById));
+router.get("/:id", isValidId, ctrlWrapper(ctrl.getById));
 
 router.post("/", validateBody(schemas.addSchema), ctrlWrapper(ctrl.add));
 
-router.delete("/:id", ctrlWrapper(ctrl.removeById));
+// router.delete("/:id", isValidId, ctrlWrapper(ctrl.removeById));
 
 router.put(
   "/:id",
+  isValidId,
   validateBody(schemas.addSchema),
   ctrlWrapper(ctrl.updateById)
+);
+
+router.patch(
+  "/:id/favorite",
+  isValidId,
+  validateBody(schemas.updatefavoriteSchema),
+  ctrlWrapper(ctrl.updateFavorite)
 );
 
 module.exports = router;
