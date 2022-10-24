36fcb53266733e30535c3c05a923146e88092173
xwinkX <opalyuk.e88@ukr.net>
2022-10-24 22:25:06 +0300

    qwwqwq

---
+++ models/user.js
@@ -0,0 +1,27 @@
+const { Schema, model } = require("mongoose");
+const Joi = require("joi");
+
+const { hendleSaveerrors } = require("../helpers");
+
+const userSchema = new Schema(
+  {
+    password: {
+      type: String,
+      required: [true, "Set password for user"],
+    },
+    email: {
+      type: String,
+      required: [true, "Email is required"],
+      unique: true,
+    },
+    subscription: {
+      type: String,
+      enum: ["starter", "pro", "business"],
+      default: "starter",
+    },
+    token: String,
+  },
+  { versionKey: false, timestamps: true }
+);
+
+usertShema.post("save", hendleSaveerrors);
