97014252d4d6fc6816f5c1939ab924ec35e97852
xwinkX <opalyuk.e88@ukr.net>
2022-11-06 18:13:34 +0200

    aaa

--- controllers/auth/updateAvatar.js
+++ controllers/auth/updateAvatar.js
@@ -1,6 +1,7 @@
 const fs = require("fs/promises");
 const path = require("path");
 const { User } = require("../../models/user");
+const jimp = require("jimp");
 
 const avatarsDir = path.join(__dirname, "../../", "public", "avatars");
 
@@ -10,8 +11,9 @@ const updateAvatar = async (req, res) => {
   const extension = originalname.split(".").pop();
   const filename = `${_id}.${extension}`;
   const resultUpload = path.join(avatarsDir, filename);
-
   await fs.rename(tmpUpload, resultUpload);
+  const file = await jimp.read(resultUpload);
+  file.resize(250, 250).write(resultUpload);
   const avatarURL = path.join("avatars", filename);
   await User.findByIdAndUpdate(_id, { avatarURL });
   res.json({
