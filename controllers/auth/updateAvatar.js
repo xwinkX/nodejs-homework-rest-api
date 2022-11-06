46b8a7851f2d92fc03793ff6b59dbe158f33849d
xwinkX <opalyuk.e88@ukr.net>
2022-11-06 13:33:39 +0200

    wwww

--- controllers/auth/updateAvatar.js
+++ controllers/auth/updateAvatar.js
@@ -7,9 +7,12 @@ const avatarsDir = path.join(__dirname, "../../", "public", "avatars");
 const updateAvatar = async (req, res) => {
   const { _id } = req.user;
   const { path: tmpUpload, originalname } = req.file;
-  const resultUpload = path.join(avatarsDir, originalname);
+  const extension = originalname.split(".").pop();
+  const filename = `${_id}.${extension}`;
+  const resultUpload = path.join(avatarsDir, filename);
+
   await fs.rename(tmpUpload, resultUpload);
-  const avatarURL = path.join("avatars", originalname);
+  const avatarURL = path.join("avatars", filename);
   await User.findByIdAndUpdate(_id, { avatarURL });
   res.json({
     avatarURL,
